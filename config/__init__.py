6dc1aaaa842daf553953e4c98e5ca4728ea56a2c
Fabianzzzl <luis.tellor@istpargentina.edu.pe>
2026-07-15 22:47:00 -0500

    Commit 3: creamos la clase

--- config/__init__.py
+++ config/__init__.py
@@ -11,3 +11,17 @@ class SistemaConfig:            # Singleton con la configuración global
             cls._inst.autor ="Tello Luis - Castro Raquel"
             Logger().info(f"Sistema Inicado :{cls._inst.nombre} Version :{cls._inst.version} Empresa :{cls._inst.empresa} Autor :{cls._inst.autor}")
         return cls._inst     
+class Alumno:
+
+    # Constructor
+    def __init__(self, codigo_alumno="", id_persona=0, id_distrito=0):
+
+        self.__id_alumno = 0
+        self.__codigo_alumno = codigo_alumno
+        self.__id_persona = id_persona
+        self.__id_distrito = id_distrito
+
+    # Getter y Setter del ID
+    @property
+    def id_alumno(self):
+        return self.__id_alumno
